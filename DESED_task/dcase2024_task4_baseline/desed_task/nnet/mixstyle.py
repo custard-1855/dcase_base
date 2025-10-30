fix mixstyle scaling

DESED_task/dcase2024_task4_baseline/desed_task/nnet/mixstyle.py
@@ -95,16 +95,17 @@ class FrequencyAttentionMixStyle(nn.Module):
         # 各周波数が独立して重要かどうかを判断するため、SoftmaxよりSigmoidが適している場合が多い
         # (B, 1, F) -> (B, 1, 1, F) に変形してブロードキャスト可能にする
         attn_weights = torch.sigmoid(attn_logits).unsqueeze(-2)
-        # attn_weights = torch.sigmoid(attn_logits).unsqueeze(-1)
+        x_attended = attn_weights * x_content
 
 
         # --- 3. MixStyleを適用 ---
         # スタイルを混ぜた特徴量を生成
-        x_mixed = mix_style(x_content)
+        # x_mixed = mix_style(x_content)
+        output = mix_style(x_attended)
 
         # --- 4. 計算した重みで元の特徴量と混ぜ合わせる ---
         # 重みが大きい周波数帯ほど、スタイルが混ざった特徴量(x_mixed)の比率が高くなる
         # x_out = (重み * スタイル適用後) + ((1 - 重み) * 元の特徴量)
-        output = attn_weights * x_mixed + (1 - attn_weights) * x_content
+        # output = attn_weights * x_mixed + (1 - attn_weights) * x_content
 
         return output