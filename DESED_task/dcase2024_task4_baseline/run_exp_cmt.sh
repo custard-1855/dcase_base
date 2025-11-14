78a13ab3f57da74d94bed35b535714e23b40c92e
custard <s1f102201278@iniad.org>
2025-11-15 00:20:18 +0900

    add unlabeld cmt sh file

--- DESED_task/dcase2024_task4_baseline/run_exp_cmt.sh
+++ DESED_task/dcase2024_task4_baseline/run_exp_cmt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 実験設定
-BASE_WANDB_DIR="cmt"
+BASE_WANDB_DIR="cmt/unlabeled/"
 
 # ログディレクトリの作成
 LOG_DIR="logs/cmt"
@@ -19,12 +19,40 @@ echo ""
 ### 全体としては300回す
 ### 7.5h
 
+
+
+################################################################################
+# 実験1: unlabeled 0
+################################################################################
+echo "[1/4] Running: sat"
+uv run train_pretrained.py \
+    --wandb_dir ${BASE_WANDB_DIR} \
+    --cmt \
+    2>&1 | tee ${LOG_DIR}${TIMESTAMP}.log
+
+echo ""
+
+
+################################################################################
+# 実験2: unlabeled 50
+################################################################################
+echo "[2/4] Running: sat"
+uv run train_pretrained.py \
+    --wandb_dir ${BASE_WANDB_DIR}50 \
+    --cmt \
+    --warmup_epochs 50 \
+    2>&1 | tee ${LOG_DIR}${TIMESTAMP}.log
+
+echo ""
+
+
+
 ################################################################################
 # 実験1: 75epoch
 ################################################################################
-echo "[1/3] Running: sat"
+echo "[3/4] Running: sat"
 uv run train_pretrained.py \
-    --wandb_dir ${BASE_WANDB_DIR}/75 \
+    --wandb_dir ${BASE_WANDB_DIR}75 \
     --cmt \
     --warmup_epochs 75 \
     2>&1 | tee ${LOG_DIR}/75${TIMESTAMP}.log
@@ -34,25 +62,12 @@ echo ""
 ################################################################################
 # 実験3: 100epoch
 ################################################################################
-echo "[2/3] Running: sat + MixStyle"
+echo "[4/4] Running: sat + MixStyle"
 uv run train_pretrained.py \
     --cmt \
     --warmup_epochs 100 \
-    --wandb_dir ${BASE_WANDB_DIR}/100 \
+    --wandb_dir ${BASE_WANDB_DIR}100 \
     2>&1 | tee ${LOG_DIR}/100_${TIMESTAMP}.log
 
 echo ""
-
-################################################################################
-# 実験3: 125epoch
-################################################################################
-echo "[3/3] Running: sat + cSEBBs"
-uv run train_pretrained.py \
-    --cmt \
-    --warmup_epochs 125 \
-    --wandb_dir ${BASE_WANDB_DIR}/125 \
-    2>&1 | tee ${LOG_DIR}/125_${TIMESTAMP}.log
-
-echo ""
-
 
