0d2a19eee811d43e9186ed3827267eea114609ef
custard <s1f102201278@iniad.org>
2025-11-20 22:03:49 +0900

    change sh file for use embeddings and mix the label on CutMix

--- DESED_task/dcase2024_task4_baseline/run_debug.sh
+++ DESED_task/dcase2024_task4_baseline/run_debug.sh
@@ -2,7 +2,7 @@
 
 # 実験設定
 MIXSTYLE_TYPE="resMix"
-BASE_WANDB_DIR="sat/debug_non-used_embeddings/cutmix_no-mix-label"
+BASE_WANDB_DIR="sat/used_embeddings/cutmix_mix-label"
 
 # ログディレクトリの作成
 LOG_DIR="logs/sat/150epoch"
