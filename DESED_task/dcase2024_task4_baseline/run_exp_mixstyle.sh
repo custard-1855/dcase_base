40edb937bab8fe3722d59ffa87169bf1b03f6bd2
custard-1855 <narian.tome.reon@gmail.com>
2025-12-04 00:25:07 +0900

    fix confs, run_mixstyle.sh: MixStyleの実装に不備が見つかったので,対照実験のため現実装を実験

--- DESED_task/dcase2024_task4_baseline/run_exp_mixstyle.sh
+++ DESED_task/dcase2024_task4_baseline/run_exp_mixstyle.sh
@@ -22,31 +22,30 @@ echo ""
 
 
 ################################################################################
-# 実験1: MixStyle + SEBBs warm-up 0
+# 実験1: MixStyle
 ################################################################################
 echo "[1/4] Running: sat"
 uv run train_pretrained.py \
-    --wandb_dir ${BASE_WANDB_DIR}/MixStyle_SEBBS \
+    --wandb_dir ${BASE_WANDB_DIR}/MixStyle_not_fix \
     --attn_type ${ATTN_TYPE} \
     --attn_deepen ${ATTN_DEEPEN} \
     --mixstyle_type ${MIXSTYLE_TYPE} \
-    --sebbs \
     2>&1 | tee ${LOG_DIR}/75${TIMESTAMP}.log
 
 echo ""
 
 
-################################################################################
-# 実験6: sat frame shift + time mask
-################################################################################
-echo "[1/4] Running: sat"
-uv run train_pretrained.py \
-    --wandb_dir sat/150/frame-shift-60_time-mask-30 \
-    --sat \
-    --strong_augment_type frame_shift_time_mask \
-    --frame_shift_std 60 \
-    --time_mask_max 30 \
-    2>&1 | tee ${LOG_DIR}/only_sat${TIMESTAMP}.log
+# ################################################################################
+# # 実験6: sat frame shift + time mask
+# ################################################################################
+# echo "[1/4] Running: sat"
+# uv run train_pretrained.py \
+#     --wandb_dir sat/150/frame-shift-60_time-mask-30 \
+#     --sat \
+#     --strong_augment_type frame_shift_time_mask \
+#     --frame_shift_std 60 \
+#     --time_mask_max 30 \
+#     2>&1 | tee ${LOG_DIR}/only_sat${TIMESTAMP}.log
 
-echo ""
+# echo ""
 
