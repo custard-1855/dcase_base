946257c2442057441f345f7e5f00af6eb3cf338b
custard-1855 <narian.tome.reon@gmail.com>
2025-12-24 20:26:06 +0900

    不要なライブラリを削除

--- DESED_task/dcase2024_task4_baseline/local/utils.py
+++ DESED_task/dcase2024_task4_baseline/local/utils.py
@@ -9,22 +9,20 @@ import pandas as pd
 import scipy
 import soundfile
 import torch
+from desed_task.evaluation.evaluation_measures import compute_sed_eval_metrics
 from sed_scores_eval.base_modules.scores import create_score_dataframe
 from thop import clever_format, profile
-
-from desed_task.evaluation.evaluation_measures import compute_sed_eval_metrics
 
 
 def process_tsvs(tsv, alias_map=None):
     if alias_map is None:
         return tsv
-    else:
-        orig_tsv = deepcopy(tsv)
-        for k in alias_map.keys():
-            if k in tsv["event_label"].unique():
-                mask = tsv["event_label"] == k
-                tsv.loc[mask, "event_label"] = alias_map[k]
-                orig_tsv = pd.concat([orig_tsv, tsv[mask]])
+    orig_tsv = deepcopy(tsv)
+    for k in alias_map.keys():
+        if k in tsv["event_label"].unique():
+            mask = tsv["event_label"] == k
+            tsv.loc[mask, "event_label"] = alias_map[k]
+            orig_tsv = pd.concat([orig_tsv, tsv[mask]])
         # use alias_map to duplicate events
         # for each entry in tsv create a new one with the classes map
         # e.g. dog_bark --> dog
@@ -52,6 +50,7 @@ def batched_decode_preds(
 
     Returns:
         dict of predictions, each keys is a threshold and the value is the DataFrame of predictions.
+
     """
     # Init a dataframe per threshold
     scores_raw = {}
@@ -86,7 +85,8 @@ def batched_decode_preds(
             pred = pd.DataFrame(pred, columns=["event_label", "onset", "offset"])
             pred["filename"] = filename
             prediction_dfs[c_th] = pd.concat(
-                [prediction_dfs[c_th], pred], ignore_index=True
+                [prediction_dfs[c_th], pred],
+                ignore_index=True,
             )
 
     return scores_raw, scores_postprocessed, prediction_dfs
@@ -101,14 +101,14 @@ def convert_to_event_based(weak_dataframe):
 
     Returns:
         pd.DataFrame, the dataframe strongly labeled.
-    """
 
+    """
     new = []
     for i, r in weak_dataframe.iterrows():
         events = r["event_labels"].split(",")
         for e in events:
             new.append(
-                {"filename": r["filename"], "event_label": e, "onset": 0, "offset": 1}
+                {"filename": r["filename"], "event_label": e, "onset": 0, "offset": 1},
             )
     return pd.DataFrame(new)
 
@@ -122,6 +122,7 @@ def log_sedeval_metrics(predictions, ground_truth, save_dir=None):
 
     Returns:
         tuple, event-based macro-F1 and micro-F1, segment-based macro-F1 and micro-F1
+
     """
     if predictions.empty:
         return 0.0, 0.0, 0.0, 0.0
@@ -153,19 +154,17 @@ def parse_jams(jams_list, encoder, out_json):
     backgrounds = []
     sources = []
     for jamfile in jams_list:
-        with open(jamfile, "r") as f:
+        with open(jamfile) as f:
             jdata = json.load(f)
 
         # check if we have annotations for each source in scaper
         assert len(jdata["annotations"][0]["data"]) == len(
-            jdata["annotations"][-1]["sandbox"]["scaper"]["isolated_events_audio_path"]
+            jdata["annotations"][-1]["sandbox"]["scaper"]["isolated_events_audio_path"],
         )
 
         for indx, sound in enumerate(jdata["annotations"][0]["data"]):
             source_name = Path(
-                jdata["annotations"][-1]["sandbox"]["scaper"][
-                    "isolated_events_audio_path"
-                ][indx]
+                jdata["annotations"][-1]["sandbox"]["scaper"]["isolated_events_audio_path"][indx],
             ).stem
             source_file = os.path.join(
                 Path(jamfile).parent,
@@ -176,9 +175,7 @@ def parse_jams(jams_list, encoder, out_json):
             if sound["value"]["role"] == "background":
                 backgrounds.append(source_file)
             else:  # it is an event
-                if (
-                    sound["value"]["label"] not in encoder.labels
-                ):  # correct different labels
+                if sound["value"]["label"] not in encoder.labels:  # correct different labels
                     if sound["value"]["label"].startswith("Frying"):
                         sound["value"]["label"] = "Frying"
                     elif sound["value"]["label"].startswith("Vacuum_cleaner"):
@@ -190,10 +187,9 @@ def parse_jams(jams_list, encoder, out_json):
                     {
                         "filename": source_file,
                         "onset": sound["value"]["event_time"],
-                        "offset": sound["value"]["event_time"]
-                        + sound["value"]["event_duration"],
+                        "offset": sound["value"]["event_time"] + sound["value"]["event_duration"],
                         "event_label": sound["value"]["label"],
-                    }
+                    },
                 )
 
     os.makedirs(Path(out_json).parent, exist_ok=True)
@@ -202,8 +198,7 @@ def parse_jams(jams_list, encoder, out_json):
 
 
 def generate_tsv_wav_durations(audio_dir, out_tsv):
-    """
-        Generate a dataframe with filename and duration of the file
+    """Generate a dataframe with filename and duration of the file
 
     Args:
         audio_dir: str, the path of the folder where audio files are (used by glob.glob)
@@ -211,6 +206,7 @@ def generate_tsv_wav_durations(audio_dir, out_tsv):
 
     Returns:
         pd.DataFrame: the dataframe containing filenames and durations
+
     """
     meta_list = []
     for file in glob.glob(os.path.join(audio_dir, "*.wav")):
@@ -224,8 +220,7 @@ def generate_tsv_wav_durations(audio_dir, out_tsv):
 
 
 def calculate_macs(model, config, dataset=None):
-    """
-    The function calculate the multiply–accumulate operation (MACs) of the model given as input.
+    """The function calculate the multiply–accumulate operation (MACs) of the model given as input.
 
     Args:
         model: deep learning model to calculate the macs for
@@ -240,7 +235,7 @@ def calculate_macs(model, config, dataset=None):
             (config["feats"]["sample_rate"] * config["data"]["audio_max_len"])
             / config["feats"]["hop_length"]
         )
-        + 1
+        + 1,
     )
     input_size = [1, config["feats"]["n_mels"], n_frames]
     input = torch.randn(input_size)
